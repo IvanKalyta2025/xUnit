ce03fe01d034e694ae1a5bb6185ef70f54da7442
IvanKalyta2025 <ivan25sfj@gmail.com>
2025-11-19 14:03:08 +0100

    first commit

---
+++ Program.cs
@@ -0,0 +1,46 @@
+﻿
+using Microsoft.VisualBasic;
+
+public interface BoxBox
+{
+    int Count(int num1, int num2);
+    void Run();
+
+
+}
+
+
+public class Box<T> : BoxBox
+{
+    public required T Content { get; set; }
+
+    public int Count(int num1, int num2)
+    {
+        int feber = num1 + num2;
+        return feber;
+    }
+
+    public void Run()
+    {
+        Console.WriteLine(Content);
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+
+        var intBox = new Box<string> { Content = "Danylo" };
+        var intBoxBool = new Box<bool> { Content = true };
+
+        intBox.Run();
+        intBoxBool.Run();
+        intBox.Count(2, 12);
+    }
+
+}
+
+
+
+
---
+++ XUNIT/ICalculable.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace xUnit.XUNIT
+{
+    public class ICalculable
+    {
+        
+    }
+}
